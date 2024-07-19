3626c4f58d6d05f69c1f43d5e7022e0047c94406
Thilaga2001 <thilagavedaraju@gmail.com>
2024-07-19 12:34:26 -0400

    created class vehicle
    created class vehicle

--- Vehicle Rental Management System/Vehicle Rental Management System/Program.cs
+++ Vehicle Rental Management System/Vehicle Rental Management System/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    public Vehicle(string model, string manufacturer, int year, decimal rentalPrice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalPrice;
+    }
+
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine($"Model: {Model}, Manufacturer: {Manufacturer}, Year: {Year}, Rental Price: {RentalPrice}");
+        return;
+    }
+}
